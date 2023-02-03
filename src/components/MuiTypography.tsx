f9dd58af6625631a8dcf407eabc792d754ba3b42
Milad1412 <milad.rahimi1412@gmail.com>
2023-02-03 11:49:28 +0330

    MuiButton

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material'
+
+const MuiTypography = () => {
+  return <div>
+    <Typography variant='h1'>h1 heading</Typography>
+  </div>
+}
+
+export default MuiTypography
